d0013dd19534b3214ba211a3bc713ba7789613f8
Nikhilsunny123 <nikhilsunny3@gmail.com>
2022-09-20 09:06:13 +0530

    Updates

---
+++ precache-manifest.b487a64a90cf42d2b391713955c08ab2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f95cf1e860029fc83027",
+    "url": "/react-gh-pages/static/css/main.42e9c4a6.chunk.css"
+  },
+  {
+    "revision": "f95cf1e860029fc83027",
+    "url": "/react-gh-pages/static/js/main.f95cf1e8.chunk.js"
+  },
+  {
+    "revision": "8db937100e6fdcd2d125",
+    "url": "/react-gh-pages/static/css/1.98644c46.chunk.css"
+  },
+  {
+    "revision": "8db937100e6fdcd2d125",
+    "url": "/react-gh-pages/static/js/1.8db93710.chunk.js"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "e31737bbff7b06ce819bc494e2035101",
+    "url": "/react-gh-pages/index.html"
+  }
+];
